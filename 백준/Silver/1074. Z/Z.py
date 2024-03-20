4123db9e1e88f3f13c44be6863a27e24f9a37d0b
Jwirang._ <103397655+Jwirang@users.noreply.github.com>
2024-03-20 10:49:37 +0900

    [Silver I] Title: Z, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.py
+++ 백준/Silver/1074. Z/Z.py
@@ -5,17 +5,20 @@ ans = 0
 while N != 0:
     N -= 1
     
-    if r < 2**N and c < 2** N:
+    if r < 2 ** N and c < 2 ** N:
         ans += (2**N) * (2**N) * 0
+
     elif r < 2 ** N and c >= 2 ** N:
         ans += (2 ** N) * (2 ** N) * 1
         c -= (2 ** N)
-    elif r >= 2 ** N and c < 2**N:
+
+    elif r >= 2 ** N and c < 2 ** N:
         ans += (2 ** N) * (2 ** N) * 2
         r -= (2 ** N)
+
     else:
         ans += (2 ** N) * (2 ** N) * 3
-        r -= (2**N)
+        r -= (2 ** N)
         c -= (2 **N)
-        
+
 print(ans)
