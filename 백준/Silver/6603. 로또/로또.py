eceb1c777087c51dda67ef23495756ed02579569
Jwirang._ <103397655+Jwirang@users.noreply.github.com>
2024-03-20 18:23:45 +0900

    [Silver II] Title: 로또, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.py
@@ -0,0 +1,16 @@
+import sys
+import itertools
+
+while True:
+
+    array = list(map(int, sys.stdin.readline().split()))
+
+    k = array[0]
+    S = array[1:]
+
+    for i in itertools.combinations(S, 6):
+        print(*i)
+
+    if k == 0:
+        exit()
+    print()
