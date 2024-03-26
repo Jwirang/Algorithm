8485a32b84e27b271c2b912bc8691a631d766b48
Jwirang._ <103397655+Jwirang@users.noreply.github.com>
2024-03-26 11:16:29 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,27 +1,24 @@
 import sys
-from collections import defaultdict
-
-sys.setrecursionlimit(10**7)
-
-def dfs(node):
-    global count
-    vistit[node] = True
-    count += 1
-    for i in graph[node]:
-        if not vistit[i]:
-            dfs(i)
-
 n = int(sys.stdin.readline())
 m = int(sys.stdin.readline())
-graph = [[] for _ in range(n+1)]
 
+graph = [[] for _ in range(n + 1)]
 for _ in range(m):
-    s, e = map(int, sys.stdin.readline().split())
-    graph[s].append(e)
-    graph[e].append(s)
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+    graph[a].sort()
+    graph[b].sort()
 
-vistit = [False] * (n + 1)
 count = 0
-dfs(1)
+visit = [False] * (n + 1)
+def dfs(node):
+    global count
+    visit[node] = True
+    count += 1
+    for i in graph[node]:
+        if visit[i] == False:
+            dfs(i)
 
+dfs(1)
 print(count - 1)
