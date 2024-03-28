4b2ab051ea54db8c760896f85a58448251231f01
Jwirang._ <103397655+Jwirang@users.noreply.github.com>
2024-03-28 10:23:33 +0900

    [Bronze IV] Title: 빵, Time: 56 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/25377. 빵/빵.py
+++ 백준/Bronze/25377. 빵/빵.py
@@ -1,13 +1,12 @@
 import sys
-
 n = int(sys.stdin.readline())
-#n_list = map(int, sys.stdin.readline().split())
+
 result = []
-for _ in range(n):
-    a, b = map(int, sys.stdin.readline().split())
-    if a < b:
-        print(b - a)
-        result.append(b)
+if n != 0:
+    for _ in range(n):
+        a, b = map(int, sys.stdin.readline().split())
+        if a <= b:
+            result.append(a + (b - a))
 
 if len(result) == 0:
     print(-1)
