ad82bf4fd20b8b60c5c78dc43df11fb0357f2982
Jwirang._ <103397655+Jwirang@users.noreply.github.com>
2024-04-01 13:53:58 +0900

    [Gold V] Title: 문자열 판별, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/16500. 문자열 판별/문자열 판별.py
@@ -0,0 +1,20 @@
+import sys
+
+s = sys.stdin.readline().strip()
+s_list = list(s)
+a_count = int(sys.stdin.readline())
+a = [sys.stdin.readline() for _ in range(a_count)]
+
+dp = [0] * (len(s_list) + 1)
+dp[0] = 1
+
+for i in range(len(s_list) + 1):
+    for j in a:
+        j_len = len(j.strip())
+        if i >= j_len and dp[i - j_len] == 1 and s_list[i - j_len:i] == list(j.strip()):
+            dp[i] = 1
+
+if dp[len(s_list)]:
+    print(1)
+else:
+    print(0)
