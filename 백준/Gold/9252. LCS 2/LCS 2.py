b00cb6770967217d25581636b2e9ec8ff7c98db2
Jwirang._ <103397655+Jwirang@users.noreply.github.com>
2024-04-01 19:48:21 +0900

    [Gold IV] Title: LCS 2, Time: 816 ms, Memory: 423724 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -0,0 +1,26 @@
+import sys
+
+S1 = sys.stdin.readline().strip()
+S2 = sys.stdin.readline().strip()
+len1 = len(S1)
+len2 = len(S2)
+matrix = [[''] * (len2 + 1) for _ in range(len1 + 1)]
+
+for i in range(1, len1 + 1):
+    for j in range(1, len2 + 1):
+        if S1[i - 1] == S2[j - 1]:
+            matrix[i][j] = matrix[i - 1][j - 1] + S1[i - 1]
+        else:
+            if len(matrix[i - 1][j]) > len(matrix[i][j - 1]):
+                matrix[i][j] = matrix[i - 1][j]
+            else:
+                matrix[i][j] = matrix[i][j - 1]
+
+# print(len(matrix[-1][-1]))
+
+# if len(matrix[-1][-1]) != 0:
+#     print(matrix[-1][-1])
+
+
+result = matrix[-1][-1]          
+print(len(result), result, sep='\n')
